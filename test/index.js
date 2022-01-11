bc4bc924098565c188f9314247944c82aa95d102
Huu Hao <huuhao1999@gmail.com>
2022-01-11 14:58:48 +0700

    fix: space string

--- test/index.js
+++ test/index.js
@@ -1,25 +1,15 @@
 import { VietQR } from '../index.js';
-// let h = new Vietqr('113366668888', 'QUY VAC XIN PHONG CHONG COVID', '970415', 'Ung Ho Quy Vac Xin', '79000', 'compact');
-let VietQR = new VietQR({
-    x_api_key: '1',
-    x_client_key: '1',
-})
-// let templates = await VietQR.getBanks()
-// console.log(templates);
-// let qrCode = await VietQR.genQRCodeSyncV2({
-//     bank: '970415',
-//     accountName: 'QUY VAC XIN PHONG CHONG COVID',
-//     accountNumber: '113366668888',
-//     amount: '79000',
-//     memo: 'Ung Ho Quy Vac Xin',
-// })
-// console.log(qrCode);
 
-let quickLink = VietQR.genQuickLink({
+let vietQR = new VietQR({
+    x_api_key: 'b9f13b6a-3779-4e2b-807f-2731cc623785',
+    x_client_key: '1f095a30-b744-43a2-8a67-080a5d25c481',
+})
+
+let quickLink = vietQR.genQuickLink({
         bank: '970415',
         accountName: 'QUY VAC XIN PHONG CHONG COVID',
         accountNumber: '113366668888',
         amount: '79000',
         memo: 'Ung Ho Quy Vac Xin',
     })
-console.log(b);
+console.log(quickLink);
