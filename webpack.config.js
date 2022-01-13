502ce32a96e00bfb2054dfd36500bc43334ae095
Huu Hao <huuhao1999@gmail.com>
2022-01-13 14:32:20 +0700

    fix: errror

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const {resolve} = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: resolve(__dirname, 'src/index.js'),
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: 'draw-vietqr.js',
+        library: 'vietqr'
+    },
+    plugins: [
+        new UglifyJsPlugin({
+            exclude: [/\.min\.js$/gi] // skip pre-minified libs
+        })
+    ]
+}
