510cab46d6115eb89f21a2022cf6914b2619311f
Huu Hao <huuhao1999@gmail.com>
2022-01-17 13:42:52 +0700

    fix: angular

--- example/qrCode.js
+++ example/qrCode.js
@@ -1,7 +1,7 @@
 let { VietQR } = require('../index');
 let vietQR = new VietQR({
     clientID: 'de8a0804-a76d-41e5-8ad6-31503ce7d5f4',
-    clientSecret: '17c29f09-4ea2-4417-b9c2-7f020d35de42',
+    apiKey: '17c29f09-4ea2-4417-b9c2-7f020d35de42',
 })
 vietQR.genQRCodeBase64({
     bank: '970415',
